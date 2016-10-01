1d8653fce6a7333edc2f12cc23ca10e8f4f154a9
bonfy <foreverbonfy@163.com>
2016-10-01 16:28:05 +0800

    update lottery

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,12 +2,10 @@
 
 from lottery.utils import (str_to_lst, dct_add_dct, dct_add_lst_with_weight)
 
-LuckyList = [1, 3, 5, 35]
-Str = '1 2 10 12 14'
-
 
 def test_str_to_lst():
-    lst = str_to_lst(Str)
+    string = '1 2 10 12 14'
+    lst = str_to_lst(string)
     assert type(lst) is list
     assert lst == [1, 2, 10, 12, 14]
 
@@ -17,6 +15,7 @@ def test_dct_add_dct():
     dct2 = {1: 3, 2: 5}
 
     dct = dct_add_dct(dct1, dct2)
+    assert type(dct) is dict
     assert dct == {1: 5, 2: 9, 3: 2, 4: 2}
 
 
@@ -26,8 +25,10 @@ def test_dct_add_lst_with_weight():
 
     # case lst < dct1.keys()
     dct = dct_add_lst_with_weight(dct1, lst, 1)
+    assert type(dct) is dict
     assert dct == {1: 3, 2: 5, 3: 3, 4: 2}
 
     # case: lst > dct1.keys()
     lst = [1, 2, 3, 5]
+    assert type(dct) is dict
     assert dct == {1: 3, 2: 5, 3: 3, 4: 2}
